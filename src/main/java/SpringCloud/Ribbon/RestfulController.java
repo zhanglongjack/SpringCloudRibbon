865ccf8dcd6ed6f867f8c8552cf3c5b387416375
zhanglongjack <364666387@qq.com>
2018-05-06 15:59:55 +0800

    test

--- src/main/java/SpringCloud/Ribbon/RestfulController.java
+++ src/main/java/SpringCloud/Ribbon/RestfulController.java
@@ -1,6 +1,5 @@
 package SpringCloud.Ribbon;
 
-
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,28 +8,39 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.test.model.ServiceInfo;
+
 @RefreshScope
 @RestController
 public class RestfulController {
-    private static final Logger logger = Logger  
-            .getLogger(RestfulController.class.getName());  
-    @Autowired private RestfulServiceImpl restfulService ;
+	private static final Logger logger = Logger.getLogger(RestfulController.class.getName());
+	@Autowired
+	private RestfulServiceImpl restfulService;
 
-    @Value("${server.port}")
-    String port ;
-    @Value("${spring.application.name}")
-    String name ;
+	@Value("${server.port}")
+	String port;
+	@Value("${spring.application.name}")
+	String name;
 
-    @RequestMapping("/home")
-    public String home(@RequestParam String message){
-    	logger.info("test========="+message);
-        return "my info from port("+port+") from application name("+name+") invoke other service." + "\n"
-                +restfulService.getRestData(message);
-    }
-    
-    @RequestMapping("/test")
-    public String test(@RequestParam String name){
-    	logger.info("test========="+name);
-        return name;
-    }
+	@RequestMapping("/home")
+	public String home(@RequestParam String message) {
+		logger.info("test=========" + message);
+		return "my info from port(" + port + ") from application name(" + name + ") invoke other service." + "\n"
+				+ restfulService.getRestData(message);
+	}
+
+	@RequestMapping("/test")
+	public String test(@RequestParam String name) {
+		logger.info("test=========" + name);
+		return name;
+	}
+
+	@RequestMapping("/info")
+	public String info(@RequestParam String name) {
+		logger.info("test=========" + name);
+		ServiceInfo info = restfulService.getMyTestInfoRestData(name);
+		return String.format("the service name is %s, and port is %s ,what's the testinfo ==%s", info.getName(),
+				info.getPort(), info.getTestInfo());
+	}
 }
--- src/main/java/SpringCloud/Ribbon/RestfulServiceImpl.java
+++ src/main/java/SpringCloud/Ribbon/RestfulServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.test.model.ServiceInfo;
+
 @Service("restfulSerivce")
 public class RestfulServiceImpl {
 
@@ -11,5 +13,9 @@ public class RestfulServiceImpl {
 
     public String getRestData(String name){
         return restTemplate.getForObject("http://spring-cloud-config-client/say?message="+name,String.class) ;
+    }
+    
+    public ServiceInfo getMyTestInfoRestData(String name){
+    	return restTemplate.getForObject("http://spring-cloud-config-client/info?message="+name,ServiceInfo.class) ;
     }
 }
